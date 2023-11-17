cbe29e4763e2a1adfee995244223c92fdbd774ef
yisroel <yisroelshulman16@gmail.com>
2023-11-16 23:21:11 -0500

    finished the print_case_result() function

--- src/result.c
+++ src/result.c
@@ -9,13 +9,26 @@ bool init_result(TestResults* testresults, const int count)
     if (testresults->results == NULL)
     {
         fprintf(stderr, "Not enough memory for the results.\n");
+        testresults->count = 0;
+        return false;
     }
     testresults->count = count;
+    return true;
 }
 
 static void print_case_result(Result result)
 {
-
+    printf("On input: %s\n", result.input);
+    switch (result.status)
+    { 
+        case PASS:
+            printf("result: pass");
+            return;
+        case FAIL:
+            printf("Expected: %s\n", result.expected);
+            printf("Received: %s\n", result.received);
+            return;
+    }
 }
 
 void print_result(const TestResults* testresults)
