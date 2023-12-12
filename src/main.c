34131bf7a81099156f58c24587bed3b86ad7735e
yisroel <yisroelshulman16@gmail.com>
2023-12-12 13:29:27 -0500

    fixed main to extrat the propper args.

--- src/main.c
+++ src/main.c
@@ -11,10 +11,10 @@ int main(int argc, char *argv[])
             start_zero();
             exit(0);
         case 2:
-            start_one(argv[2]);
+            start_one(argv[1]);
             exit(2);
         case 3:
-            start_two(argv[2], argv[3]);
+            start_two(argv[1], argv[2]);
             exit(3);
         default:
             fprintf(stderr, "[%d] invalid number of args.\n", argc);
